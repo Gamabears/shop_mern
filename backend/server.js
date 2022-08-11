b374a02c9c8b84860c5bd32b8389523ab99ab361
Gamabears <871400768@qq.com>
2022-08-10 19:13:20 -0700

    Prepare for deploy

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 8000
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const path = require('path')
 
 
 //Connect to DB
@@ -12,13 +13,23 @@ connectDB()
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 
-app.get('/',(req,res)=>{
-    res.send('Hello')
-})
+
 
 //Routes
 app.use('/api/users',require('./routes/userRoutes'))
 app.use('/api/tickets',require('./routes/ticketRoutes'))
+
+// Serve Frontend
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../frontend/build')))
+    app.get('*',(req,res)=>res.sendFile(__dirname, '../','frontend','build','index.html'))
+}
+else{
+    app.get('/',(req,res)=>{
+        res.status(200).json({message: 'Welcome to the Support Desk API'})
+    })
+}
 
 app.use(errorHandler)
 app.listen(PORT,()=>console.log(`Server started on port ${PORT}`))
